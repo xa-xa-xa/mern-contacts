c851e39612b2d3c1d580d87ce7766f4f99388652
xa-xa-xa <askrasn@gmail.com>
2019-10-29 09:35:43 -0700

    starting with user model

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
 /**
  * @route POST api/users
  * @desc Register a user
  * @access Public
  */
 router.post('/', (req, res) => {
-  res.send('Register a user');
+  res.send('Register a user: ', req.body);
 });
 
 module.exports = router;
