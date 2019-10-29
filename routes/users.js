d5fc70d0243b9b5b228c540db23efe0fa0e3ed31
xa-xa-xa <askrasn@gmail.com>
2019-10-29 10:47:55 -0700

    jwt+git ignore

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 /**
  * @route POST api/users
@@ -35,9 +37,25 @@ router.post(
       user = new User({ name, email, password });
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+
       await user.save();
 
-      res.send('user saved');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000 // usually 36000 (1hour)
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server ERROR!');
