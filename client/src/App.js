e784a9957dd36fa479e50cea6232975b581bc0e4
xa-xa-xa <askrasn@gmail.com>
2019-11-05 08:36:11 -0800

    fix typo

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
-import AuthState from './context/auth/AuthState';
+import AuthState from './context/auth/authState';
 import ContactState from './context/contact/ContactState';
 
 const App = () => {
