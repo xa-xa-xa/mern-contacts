4f72bb11c50ea1e0309e87cf73f8775046aed6a5
xa-xa-xa <askrasn@gmail.com>
2019-11-12 10:56:34 -0800

    clg

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,24 +1,23 @@
-import React, { useContext, useEffect } from 'react';
-import Contacts from '../contacts/Contacts';
-import ContactForm from '../contacts/ContactForm';
-import ContactFilter from '../contacts/contactFilter';
-import AuthContext from '../../context/auth/authContext';
+import React, { useContext, useEffect } from "react";
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+import ContactFilter from "../contacts/contactFilter";
+import AuthContext from "../../context/auth/authContext";
 
 const Home = () => {
   const authContext = useContext(AuthContext);
 
   useEffect(() => {
     authContext.loadUser();
-    console.log('!!!! Home.js: ', authContext);
     // eslint-disable-next-line
   }, []);
 
   return (
-    <div className='grid-2'>
+    <div className="grid-2">
       <ContactForm />
       <div>
-        {' '}
-        <h2 className='text-left text-center'>Contacts List:</h2>
+        {" "}
+        <h2 className="text-left text-center">Contacts List:</h2>
         <ContactFilter />
         <Contacts />
       </div>
