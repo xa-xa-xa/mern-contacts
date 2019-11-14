96c7a7199e61f7efd3a6fa337b421749ad1fcb39
xa-xa-xa <askrasn@gmail.com>
2019-11-14 07:38:22 -0800

    preparing for deploy

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const connectDB = require('./config/db');
-const PORT = process.env.PORT || 5000;
 
 // Connect Database
 connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Hello from Contacts!' }));
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`*** Server listening on port: ${PORT}`));
